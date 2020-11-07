8ca179560b342d620ecb2124101b381f893be0f7
Rama Tri Agung <ramatriagung91@gmail.com>
2020-11-07 22:09:10 +0700

    finishing DetailMovieViewModelTest.kt

--- app/src/test/java/com/example/moviecatalogue/ui/detail/movie/DetailMovieViewModelTest.kt
+++ app/src/test/java/com/example/moviecatalogue/ui/detail/movie/DetailMovieViewModelTest.kt
@@ -1,9 +1,21 @@
 package com.example.moviecatalogue.ui.detail.movie
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.example.moviecatalogue.data.Movie
+import com.example.moviecatalogue.data.source.MovieRepository
 import com.example.moviecatalogue.utils.DataDummy
+import com.nhaarman.mockitokotlin2.verify
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
 /*
 Scenario DetailMovieViewModelTest :
@@ -12,29 +24,48 @@ Scenario DetailMovieViewModelTest :
         - Memastikan data movie sesuai dengan yang diharapkan
  */
 
+@RunWith(MockitoJUnitRunner::class)
 class DetailMovieViewModelTest {
     private lateinit var viewModel: DetailMovieViewModel
     private val dummyMovie = DataDummy.generateDummyMovie()[0]
     private val dummyMovieId = dummyMovie.id
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var movieRepository: MovieRepository
+
+    @Mock
+    private lateinit var movieObserver: Observer<Movie>
+
     @Before
     fun setUp() {
-        viewModel = DetailMovieViewModel()
+        viewModel = DetailMovieViewModel(movieRepository)
         dummyMovieId?.let { viewModel.setMovieId(it) }
     }
 
     @Test
-    fun getDetailMovie() {
-        dummyMovie.id?.let { viewModel.setMovieId(it) }
-        val movie = viewModel.getMovie()
-        assertNotNull(movie)
-        assertEquals(dummyMovie.id, movie?.id)
-        assertEquals(dummyMovie.originalTitle, movie?.originalTitle)
-        assertEquals(dummyMovie.title, movie?.title)
-        assertEquals(dummyMovie.overview, movie?.overview)
-        assertEquals(dummyMovie.backdropPath, movie?.backdropPath)
-        assertEquals(dummyMovie.posterPath, movie?.posterPath)
-        assertEquals(dummyMovie.releaseDate, movie?.releaseDate)
-        assertEquals(dummyMovie.voteAverage, movie?.voteAverage)
+    fun getMovie() {
+        val movieLive = MutableLiveData<Movie>()
+        movieLive.value = dummyMovie
+
+        if (dummyMovieId != null) {
+            `when`(movieRepository.getMovie(dummyMovieId)).thenReturn(movieLive)
+            val movie = viewModel.getMovie().value as Movie
+            verify(movieRepository).getMovie(dummyMovieId)
+            assertNotNull(movie)
+            assertEquals(dummyMovie.id, movie.id)
+            assertEquals(dummyMovie.originalTitle, movie.originalTitle)
+            assertEquals(dummyMovie.title, movie.title)
+            assertEquals(dummyMovie.overview, movie.overview)
+            assertEquals(dummyMovie.backdropPath, movie.backdropPath)
+            assertEquals(dummyMovie.posterPath, movie.posterPath)
+            assertEquals(dummyMovie.releaseDate, movie.releaseDate)
+            assertEquals(dummyMovie.voteAverage, movie.voteAverage)
+
+            viewModel.getMovie().observeForever(movieObserver)
+            Mockito.verify(movieObserver).onChanged(dummyMovie)
+        }
     }
 }
