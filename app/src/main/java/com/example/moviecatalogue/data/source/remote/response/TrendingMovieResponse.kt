e5e313f594c01d7da788750794d6a2e79a58509a
Rama Tri Agung <ramatriagung91@gmail.com>
2020-11-07 21:21:17 +0700

    create dummy data remote and fixing trending response

--- app/src/main/java/com/example/moviecatalogue/data/source/remote/response/TrendingMovieResponse.kt
+++ app/src/main/java/com/example/moviecatalogue/data/source/remote/response/TrendingMovieResponse.kt
@@ -19,48 +19,30 @@ data class TrendingMovieResponse(
 
 data class TrendingMovieResultsItem(
 
-	@field:SerializedName("overview")
-	val overview: String,
+    @field:SerializedName("overview")
+    val overview: String,
 
-	@field:SerializedName("original_language")
-	val originalLanguage: String,
+    @field:SerializedName("original_title")
+    val originalTitle: String,
 
-	@field:SerializedName("original_title")
-	val originalTitle: String,
+    @field:SerializedName("title")
+    val title: String,
 
-	@field:SerializedName("video")
-	val video: Boolean,
+    @field:SerializedName("poster_path")
+    val posterPath: String,
 
-	@field:SerializedName("title")
-	val title: String,
+    @field:SerializedName("backdrop_path")
+    val backdropPath: String,
 
-	@field:SerializedName("genre_ids")
-	val genreIds: List<Int>,
+    @field:SerializedName("release_date")
+    val releaseDate: String,
 
-	@field:SerializedName("poster_path")
-	val posterPath: String,
+    @field:SerializedName("vote_average")
+    val voteAverage: Double,
 
-	@field:SerializedName("backdrop_path")
-	val backdropPath: String,
+    @field:SerializedName("id")
+    val id: String,
 
-	@field:SerializedName("release_date")
-	val releaseDate: String,
-
-	@field:SerializedName("media_type")
-	val mediaType: String,
-
-	@field:SerializedName("vote_average")
-	val voteAverage: Double,
-
-	@field:SerializedName("popularity")
-	val popularity: Double,
-
-	@field:SerializedName("id")
-	val id: String,
-
-	@field:SerializedName("adult")
-	val adult: Boolean,
-
-	@field:SerializedName("vote_count")
-	val voteCount: Int
+    @field:SerializedName("vote_count")
+    val voteCount: Int
 )
--- app/src/main/java/com/example/moviecatalogue/utils/DataDummy.kt
+++ app/src/main/java/com/example/moviecatalogue/utils/DataDummy.kt
@@ -2,6 +2,8 @@ package com.example.moviecatalogue.utils
 
 import com.example.moviecatalogue.data.Movie
 import com.example.moviecatalogue.data.TvShow
+import com.example.moviecatalogue.data.source.remote.response.TrendingMovieResultsItem
+import com.example.moviecatalogue.data.source.remote.response.TrendingTvShowResultsItem
 
 object DataDummy {
 
@@ -249,6 +251,280 @@ object DataDummy {
                 id = "99807",
                 name = "La Révolution",
                 voteAverage = 7.2,
+                firstAirDate = "2020-10-16",
+                posterPath = "/7OXN9nUI5sV1AkBViSKeehoePoG.jpg",
+                backdropPath = "/lpRROknTUlvJ8gPgmRUlIoOVCdA.jpg",
+                overview = "1787, France. While investigating a series of mysterious murders, Joseph Guillotin - the future inventor of the world famous ‘Guillotine’ - uncovers an unknown virus: the Blue Blood. The disease quickly spreads amongst the French aristocracy, driving them to murder ordinary people and soon leads to a rebellion."
+            )
+        )
+
+        return tvshows
+    }
+
+    fun generateRemoteDummyMovie(): List<TrendingMovieResultsItem> {
+        val movies = ArrayList<TrendingMovieResultsItem>()
+
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "740985",
+                voteAverage = 6.8,
+                voteCount = 468,
+                title = "Borat Subsequent Moviefilm",
+                releaseDate = "2020-10-23",
+                originalTitle = "Borat Subsequent Moviefilm",
+                backdropPath = "/3Ysvp4ODDC6sucdQ9quHWkMiKED.jpg",
+                posterPath = "/6agKYU5IQFpuDyUYPu39w7UCRrJ.jpg",
+                overview = "14 years after making a film about his journey across the USA, Borat risks life and limb when he returns to the United States with his young daughter, and reveals more about the culture, the COVID-19 pandemic, and the political elections."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "531219",
+                voteAverage = 7.2,
+                voteCount = 4628,
+                title = "Roald Dahl's The Witches",
+                releaseDate = "2020-10-26",
+                originalTitle = "Roald Dahl's The Witches",
+                backdropPath = "/v5uzuxtegkel1nNHdk5SHot6dlT.jpg",
+                posterPath = "/betExZlgK0l7CZ9CsCBVcwO1OjL.jpg",
+                overview = "In late 1967, a young orphaned boy goes to live with his loving grandma in the rural Alabama town of Demopolis. As the boy and his grandmother encounter some deceptively glamorous but thoroughly diabolical witches, she wisely whisks him away to a seaside resort. Regrettably, they arrive at precisely the same time that the world's Grand High Witch has gathered."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "105",
+                voteAverage = 8.3,
+                voteCount = 88,
+                title = "Back to the Future",
+                releaseDate = "1985-07-03",
+                originalTitle = "Back to the Future",
+                backdropPath = "/fq3wyOs1RHyz2yfzsb4sck7aWRG.jpg",
+                posterPath = "/xlBivetfrtF84Yx0zISShnNtHYe.jpg",
+                overview = "Eighties teenager Marty McFly is accidentally sent back in time to 1955, inadvertently disrupting his parents' first meeting and attracting his mother's romantic interest. Marty must repair the damage to history by rekindling his parents' romance and - with the help of his eccentric inventor friend Doc Brown - return to 1985."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "590223",
+                voteAverage = 7.6,
+                voteCount = 123,
+                title = "Love and Monsters",
+                releaseDate = "2020-10-16",
+                originalTitle = "Love and Monsters",
+                backdropPath = "/lA5fOBqTOQBQ1s9lEYYPmNXoYLi.jpg",
+                posterPath = "/r4Lm1XKP0VsTgHX4LG4syAwYA2I.jpg",
+                overview = "Seven years after the Monsterpocalypse, Joel Dawson, along with the rest of humanity, has been living underground ever since giant creatures took control of the land. After reconnecting over radio with his high school girlfriend Aimee, who is now 80 miles away at a coastal colony, Joel begins to fall for her again. As Joel realizes that there’s nothing left for him underground, he decides against all logic to venture out to Aimee, despite all the dangerous monsters that stand in his way."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "625568",
+                voteAverage = 6.4,
+                voteCount = 9,
+                title = "Unhinged",
+                releaseDate = "2020-07-02",
+                originalTitle = "Unhinged",
+                backdropPath = "/gvEAq0gaEqWZ2ifPW7j8jGwF69M.jpg",
+                posterPath = "/6WC3cIhuaLZSq3SbtdOp2ZTSTe.jpg",
+                overview = "A divorced mother honks impatiently at a deranged middle-aged stranger at a red light while running late on her way to work. His road rage escalates to horrifyingly psychotic proportions as he becomes single-mindedly determined to teach her a deadly lesson for provoking him."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "560050",
+                voteAverage = 7.9,
+                voteCount = 27,
+                title = "Over the Moon",
+                releaseDate = "2020-10-16",
+                originalTitle = "Over the Moon",
+                backdropPath = "/htBUhLSS7FfHtydgYxUWjL3J1Q1.jpg",
+                posterPath = "/lQfdytwN7eh0tXWjIiMceFdBBvD.jpg",
+                overview = "A girl builds a rocket to travel to the moon in hopes of meeting the legendary Moon Goddess."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "556984",
+                voteAverage = 7.8,
+                voteCount = 738,
+                title = "The Trial of the Chicago 7",
+                releaseDate = "2020-09-25",
+                originalTitle = "The Trial of the Chicago 7",
+                backdropPath = "/v8Nf6Y1qL1Q3PWTBezXNPPaXqza.jpg",
+                posterPath = "/ahf5cVdooMAlDRiJOZQNuLqa1Is.jpg",
+                overview = "What was intended to be a peaceful protest at the 1968 Democratic National Convention turned into a violent clash with police and the National Guard. The organizers of the protest — including Abbie Hoffman, Jerry Rubin, Tom Hayden and Bobby Seale — were charged with conspiracy to incite a riot and the trial that followed was one of the most notorious in history."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "505379",
+                voteAverage = 6.3,
+                voteCount = 2352,
+                title = "Rebecca",
+                releaseDate = "2020-10-16",
+                originalTitle = "Rebecca",
+                backdropPath = "/mY8oTSMlVHgkBtKsvanKnvT8rY9.jpg",
+                posterPath = "/bSKVKcCXdKxkbgf0LL8lBTPG02e.jpg",
+                overview = "After a whirlwind romance with a wealthy widower, a naïve bride moves to his family estate but can't escape the haunting shadow of his late wife."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "613504",
+                voteAverage = 6.9,
+                voteCount = 987,
+                title = "After We Collided",
+                releaseDate = "2020-09-02",
+                originalTitle = "After We Collided",
+                backdropPath = "/6hgItrYQEG33y0I7yP2SRl2ei4w.jpg",
+                posterPath = "/kiX7UYfOpYrMFSAGbI6j1pFkLzQ.jpg",
+                overview = "Tessa finds herself struggling with her complicated relationship with Hardin; she faces a dilemma that could change their lives forever."
+            )
+        )
+        movies.add(
+            TrendingMovieResultsItem(
+                id = "575417",
+                voteAverage = 6.5,
+                voteCount = 33,
+                title = "On the Rocks",
+                releaseDate = "2020-10-02",
+                originalTitle = "On the Rocks",
+                backdropPath = "/7FQHLt4iNh2TZ58cAAYrZK0xogg.jpg",
+                posterPath = "/fcijRCmB7yTtloh4Pumy9b1rkwU.jpg",
+                overview = "Faced with sudden doubts about her marriage, a young New York mother teams up with her larger-than-life playboy father to tail her husband."
+            )
+        )
+
+        return movies
+    }
+
+    fun generateRemoteDummyTvShow(): List<TrendingTvShowResultsItem> {
+        val tvshows = ArrayList<TrendingTvShowResultsItem>()
+
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                backdropPath = "/iU955nBcsc5Zr5e31eWU2xKVqHk.jpg",
+                firstAirDate = "2020-10-23",
+                id = "93785",
+                name = "Barbarians",
+                voteCount = 345,
+                originalName = "Barbaren",
+                overview = "Three people's fates are interwoven in the Battle of the Teutoburg Forest in 9 A.D., during which Germanic warriors halt the spread of the Roman Empire.",
+                posterPath = "/trfHliK2hhw2xOJVztHqKylyzrD.jpg",
+                voteAverage = 7.3
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "The Haunting of Bly Manor",
+                id = "109958",
+                name = "The Haunting of Bly Manor",
+                voteCount = 32,
+                voteAverage = 7.8,
+                firstAirDate = "2020-10-09",
+                posterPath = "/vIXQ8UymmQ7zJEPrKJP3s3fSbhR.jpg",
+                backdropPath = "/isevY1UDcpaAYlYo7IqoSAU9s81.jpg",
+                overview = "After an au pair’s tragic death, Henry Wingrave hires a young American nanny to care for his orphaned niece and nephew who reside at Bly Manor with the estate’s chef Owen, groundskeeper Jamie and housekeeper, Mrs. Grose. But all is not as it seems at the manor, and centuries of dark secrets of love and loss are waiting to be unearthed in this chilling gothic romance. At Bly Manor, dead doesn’t mean gone."
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "The Queen's Gambit",
+                id = "87739",
+                name = "The Queen's Gambit",
+                voteAverage = 9.2,
+                voteCount = 3453,
+                firstAirDate = "2020-10-23",
+                posterPath = "/zU0htwkhNvBQdVSIKB9s6hgVeFK.jpg",
+                backdropPath = "/34OGjFEbHj0E3lE2w0iTUVq0CBz.jpg",
+                overview = "In a Kentucky orphanage in the 1950s, a young girl discovers an astonishing talent for chess while struggling with addiction."
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "The Boys",
+                id = "76479",
+                name = "The Boys",
+                voteAverage = 8.4,
+                voteCount = 235,
+                firstAirDate = "2019-07-25",
+                posterPath = "/mY7SeH4HFFxW1hiI6cWuwCRKptN.jpg",
+                backdropPath = "/mGVrXeIjyecj6TKmwPVpHlscEmw.jpg",
+                overview = "A group of vigilantes known informally as “The Boys” set out to take down corrupt superheroes with no more than blue-collar grit and a willingness to fight dirty."
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "The Walking Dead: World Beyond",
+                id = "94305",
+                name = "The Walking Dead: World Beyond",
+                voteAverage = 7.7,
+                voteCount = 59,
+                firstAirDate = "2020-10-04",
+                posterPath = "/z31GxpVgDsFAF4paZR8PRsiP16D.jpg",
+                backdropPath = "/pLVrN9B750ehwTFdQ6n3HRUERLd.jpg",
+                overview = "A heroic group of teens sheltered from the dangers of the post-apocalyptic world leave the safety of the only home they have ever known and embark on a cross-country journey to find the one man who can possibly save the world."
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "Star Trek: Discovery",
+                id = "67198",
+                name = "Star Trek: Discovery",
+                voteAverage = 7.0,
+                voteCount = 90,
+                firstAirDate = "2017-09-24",
+                posterPath = "/98RYSYsRNKWgrAAFBn0WfploUG7.jpg",
+                backdropPath = "/p3McpsDNTNmpbkNBKdNxOFZJeKX.jpg",
+                overview = "Follow the voyages of Starfleet on their missions to discover new worlds and new life forms, and one Starfleet officer who must learn that to truly understand all things alien, you must first understand yourself."
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "Fear the Walking Dead",
+                id = "62286",
+                name = "Fear the Walking Dead",
+                voteAverage = 7.3,
+                voteCount = 34523,
+                firstAirDate = "2015-08-23",
+                posterPath = "/wGFUewXPeMErCe2xnCmmLEiHOGh.jpg",
+                backdropPath = "/58PON1OrnBiX6CqEHgeWKVwrCn6.jpg",
+                overview = "What did the world look like as it was transforming into the horrifying apocalypse depicted in \"The Walking Dead\"? This spin-off set in Los Angeles, following new characters as they face the beginning of the end of the world, will answer that question."
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "Helstrom",
+                id = "88987",
+                name = "Helstrom",
+                voteAverage = 6.6,
+                voteCount = 40,
+                firstAirDate = "2020-10-16",
+                posterPath = "/8fPIcaRIZgsoBcgA5yAm3GNz61M.jpg",
+                backdropPath = "/qCReQONjNVaW4QFN2V5vCOzPvex.jpg",
+                overview = "Daimon and Ana Helstrom are the son and daughter of a mysterious and powerful serial killer. The siblings have a complicated dynamic as they track down the terrorizing worst of humanity — each with their attitude and skills."
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "The Undoing",
+                id = "83851",
+                name = "The Undoing",
+                voteAverage = 4.0,
+                voteCount = 10,
+                firstAirDate = "2020-10-25",
+                posterPath = "/3tDbJxobPN3EI2bBebL6zmusmw5.jpg",
+                backdropPath = "/tJjwEzTglpxoWJXwEbZlKLppMhr.jpg",
+                overview = "Grace Fraser is a successful therapist on the brink of publishing her first book with a devoted husband and young son who attends an elite private school in New York City. Weeks before her book is published, a chasm opens in her life: a violent death, a missing husband, and, in the place of a man Grace thought she knew, only a chain of terrible revelations."
+            )
+        )
+        tvshows.add(
+            TrendingTvShowResultsItem(
+                originalName = "La Révolution",
+                id = "99807",
+                name = "La Révolution",
+                voteAverage = 7.2,
+                voteCount = 19,
                 firstAirDate = "2020-10-16",
                 posterPath = "/7OXN9nUI5sV1AkBViSKeehoePoG.jpg",
                 backdropPath = "/lpRROknTUlvJ8gPgmRUlIoOVCdA.jpg",
